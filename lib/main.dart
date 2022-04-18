e2900cee8842a223fcde952aad532163f09831ea
GabrielFdoVal <gabrielfdoval1@gmail.com>
2022-04-18 18:48:58 -0300

    First commit, added a description on "README.md" And created the main() with some titles

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+main() => runApp(ExpensesApp());
+
+class ExpensesApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: MyHome(),
+    );
+  }
+}
+
+class MyHome extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Despesas Pessoais"),
+      ),
+      body: Center(
+        child: Text("Visão inicial"),
+      ),
+    );
+  }
+}
