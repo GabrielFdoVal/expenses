25850c8ff4284869dc2310179623194670496f0c
GabrielFdoVal <gabrielfdoval1@gmail.com>
2022-04-18 21:03:57 -0300

    Created the transaction class, that is responsible to be a list of transactions
    Then added to the main class with two itens, in the bottom position
    for the end I created a Row for each item in transations list

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:expenses/models/transaction.dart';
 import 'package:flutter/material.dart';
 
 main() => runApp(ExpensesApp());
@@ -6,20 +7,62 @@ class ExpensesApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: MyHome(),
+      home: MyHomePage(),
     );
   }
 }
 
-class MyHome extends StatelessWidget {
+class MyHomePage extends StatelessWidget {
+  final _transactions = [
+    Transation(
+      id: 't1',
+      title: 'Novo tênis corrida',
+      value: 310.76,
+      date: DateTime.now(),
+    ),
+    Transation(
+      id: 't2',
+      title: 'Conta de luz',
+      value: 211.79,
+      date: DateTime.now(),
+    )
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("Despesas Pessoais"),
       ),
-      body: Center(
-        child: Text("Visão inicial"),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          Card(
+            child: Text("Gráfico"),
+            elevation: 5,
+          ),
+          Column(
+            children: _transactions.map(
+              (tr) {
+                return Card(
+                    child: Row(
+                  children: <Widget>[
+                    Container(
+                      child: Text(tr.value.toString()),
+                    ),
+                    Column(
+                      children: <Widget>[
+                        Text(tr.title),
+                        Text(tr.date.toString()),
+                      ],
+                    )
+                  ],
+                ));
+              },
+            ).toList(),
+          )
+        ],
       ),
     );
   }
